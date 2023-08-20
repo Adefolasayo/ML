ca6fa63fce8a79e773e07fb60b8646a2ac29d227
Gboyega-adejuwon Adefolasayo Ruth <100368706+Adefolasayo@users.noreply.github.com>
2023-08-21 00:25:06 +0100

    Add files via upload

--- bot/bot.py
+++ bot/bot.py
@@ -3,44 +3,47 @@ import long_responses as long
 
 
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
-    message_certainty=0
-    has_required_words=True
+    message_certainty = 0
+    has_required_words = True
 
-    #counts how many words are in each predefined message
+    # Counts how many words are present in each predefined message
     for word in user_message:
         if word in recognised_words:
-            message_certainty +=1
+            message_certainty += 1
 
-    #calculates the percentage of recognised words in a user message
+    # Calculates the percent of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
-    
-    #checks that the required words are in the string
+
+    # Checks that the required words are in the string
     for word in required_words:
         if word not in user_message:
             has_required_words = False
             break
 
-        if has_required_words or single_response:
-            return int(percentage*100)
-        else:
-            return 0
-
+    # Must either have the required words, or be a single response
+    if has_required_words or single_response:
+        return int(percentage * 100)
+    else:
+        return 0
 
 
 def check_all_messages(message):
-    highest_prob_list={}
+    highest_prob_list = {}
 
+    # Simplifies response creation / adds it to the dict
     def response(bot_response, list_of_words, single_response=False, required_words=[]):
         nonlocal highest_prob_list
-        highest_prob_list[bot_response]=message_probability(message, list_of_words, single_response, required_words)
-
-        #Response------------------------------,---------------------------------------------
-        response('Hello', ['hello', 'hi', 'sup', 'hey', 'heyo'], single_response=True)
-        response('I\'m doing fine and you? ', ['how', 'are', 'you', 'doing'], required_words=['how'])
-        response('Thank you', ['i', 'love', 'you', 'code', 'like', 'palace'], required_words=['code', 'palace'])
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
 
 
-        # Longer responses
+   # Responses -------------------------------------------------------------------------------------------------------
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
+    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
+    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+
+    # Longer 
     response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
     response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
 
@@ -50,16 +53,14 @@ def check_all_messages(message):
 
     return long.unknown() if highest_prob_list[best_match] < 1 else best_match
 
-       
-       
 
+# Used to get the response
 def get_response(user_input):
-    split_message = re.split( r'\s+|[,;?!.-]\s*', user_input.lower())
-    response=check_all_messages(split_message)
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    response = check_all_messages(split_message)
     return response
 
 
-
-#testing the response system
+# Testing the response system
 while True:
     print('Bot: ' + get_response(input('You: ')))
